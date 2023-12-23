58e195d2e01a77448328653870219c219c3988b3
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2023-12-23 11:45:50 -0800

    merge main

---
+++ src/BasicAuthenticationSample/BasicAuthenticationExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using BasicAuthorizationSample;
+using Microsoft.AspNetCore.Authentication;
+
+namespace BasicAuthenticationSample;
+
+public static class BasicAuthenticationExtensions
+{
+  public static AuthenticationBuilder AddBasicAuthentication(this AuthenticationBuilder builder, Action<BasicAuthenticationOptions> configureOptions)
+  {
+    ArgumentNullException.ThrowIfNull(builder);
+    ArgumentNullException.ThrowIfNull(configureOptions);
+
+    builder.AddScheme<BasicAuthenticationOptions, BasicAuthenticationHandler>(BasicAuthenticationDefaults.AuthenticationScheme, null, configureOptions);
+
+    return builder;
+  }
+}
